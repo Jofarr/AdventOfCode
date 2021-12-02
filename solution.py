29558c0a30e95cd049b638d22a9364fd1d0d3cfc
Jofarr <61031716+Jofarr@users.noreply.github.com>
2021-12-02 00:42:19 -0500

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,15 @@
+f = [x.split(" ") for x in open("02.txt").read().strip().split("\n")]
+x = prob1y = y = aim = 0
+for d, m in f:
+    m = int(m)
+    if d[0] == "f":
+        x += m
+        y += m * aim
+    elif d[0] == "d":
+        aim += m
+        prob1y += m
+    else:
+        aim -= m
+        prob1y -= m
+print(x * prob1y)
+print(x * y)
